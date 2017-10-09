sugarlabs/terminal-activity

commit 9e5a4c4f045de8f1ab9e35580b57c1d3dd282201

Font size, keep size on theme change
When theme was changed between dark or light, the font size would reset
back to default.

During VT reconfigure, keep the font size as it was, by allowing the new
font to default to the previous font size.

Also make a font size default instead of inheriting the Pango default.

terminal.py
@@ -59,6 +59,7 @@ log = logging.getLogger('Terminal')
 log.setLevel(logging.DEBUG)
 logging.basicConfig()
 
+FONT_SIZE = 10
 
 VTE_VERSION = 0
 try:
@@ -81,6 +82,7 @@ class TerminalActivity(activity.Activity):
         self.max_participants = 1
 
         self._theme_state = "light"
+        self._font_size = FONT_SIZE
 
         toolbar_box = ToolbarBox()
 
@@ -558,8 +560,15 @@ class TerminalActivity(activity.Activity):
         else:
             conf.add_section('terminal')
 
+        font_desc = vt.get_font()
+        if font_desc is None:
+            font_size = self._font_size * Pango.SCALE
+        else:
+            font_size = font_desc.get_size()
         font = self._get_conf(conf, 'font', 'Monospace')
-        vt.set_font(Pango.FontDescription(font))
+        font_desc = Pango.FontDescription(font)
+        font_desc.set_size(font_size)
+        vt.set_font(font_desc)
 
         self._theme_colors = {"light": {'fg_color': '#000000',
                                         'bg_color': '#FFFFFF'},

commit 90b5a0ed9b5a921f536c9e1e8157bc3149fe6566

Set font size default for Fedora 18
In Terminal-44.1 on Fedora 18 the column width of the terminal is much
less than 80 columns, so the multi-lingual and Klingon greeting does not
render properly.

Regression caused by 9e5a4c4f045de8f1ab9e35580b57c1d3dd282201.

terminal.py
@@ -64,7 +64,15 @@ log = logging.getLogger('Terminal')
 log.setLevel(logging.DEBUG)
 logging.basicConfig()
 
-FONT_SIZE = 10
+try:
+    olpc_build = file('/boot/olpc_build', 'r').readline()
+except:
+    olpc_build = ''
+
+if olpc_build.startswith('13'):
+    FONT_SIZE = 8
+else:
+    FONT_SIZE = 12
 
 VTE_VERSION = 0
 try: