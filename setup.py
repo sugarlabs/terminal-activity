71aa736c436871bf883c474a8559188b85cff40a
mostlykiguess <bruvistrue93@gmail.com>
2026-01-04 18:51:35 +0530

    Update activity metadata and setup for GTK4
    - Bump activity_version from 47 to 48
    - Change exec from sugar-activity3 to sugar-activity4

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 # Copyright (C) 2006, Red Hat, Inc.
+# Copyright (C) 2025 MostlyK
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -16,6 +17,6 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
-from sugar3.activity import bundlebuilder
+from sugar4.activity import bundlebuilder
 
 bundlebuilder.start()
